b45e28bbb3fbb55b9a5600708ea320d198ca080a
seunkoko <seunowonikoko@gmail.com>
2018-05-25 19:01:20 +0100

    Deploy to heroku

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![CircleCI](https://circleci.com/gh/seunkoko/bank_application_api.svg?style=svg)](https://circleci.com/gh/seunkoko/bank_application_api)
+
 # bank_application_api
 API for a simple bank application
--- main.py
+++ main.py
@@ -102,4 +102,5 @@ app = create_flask_app(os.getenv('FLASK_CONFIG'))
 
 # starts the flask application
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
